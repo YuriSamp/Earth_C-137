c76a710291b1c473951f0ac1d625a2e5938271bf
YuriSamp <yurisamp123@gmail.com>
2022-11-06 19:42:48 -0300

    maybe this time

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -9,60 +9,60 @@ import { darkMode } from 'state/atom';
 
 export const Home = () => {
 
+  // variaveis para a  Querry 
+
   const [Obj, setObj] = useState<IPersonagens[]>([]);
   const [FiltroStatus, setFiltroStatus] = useState('');
   const [FiltroSpecies, setFiltroSpecies] = useState('');
   const [Nome, setNome] = useState('');
   const [MaisPersonagens, setMaisPersonagens] = useState(true);
-  const [SemPersonagens, setSemPersonagens] = useState(false);
   const [Page, setPage] = useState(1);
-  const DarkMode = useRecoilValue(darkMode);
   const [contador, setContador] = useState(0);
   const [contadorObjetos, setContadorObjetos] = useState(20);
 
-  useEffect(() => {
+  // o resto 
+  
+  const DarkMode = useRecoilValue(darkMode);
 
-    http.get(`character/?page=${Page}`)
-      .then(resp => {
-        setObj(resp.data.results);
-        setContador(resp.data.info.pages);
-        setContadorObjetos(resp.data.info.count);
-      });
+  useEffect(() => {
+    if (FiltroSpecies === '' && FiltroStatus === '' && Nome === '') {
+      http.get(`character/?page=${Page}`)
+        .then(resp => {
+          setObj(resp.data.results);
+          setContador(resp.data.info.pages);
+          setContadorObjetos(resp.data.info.count);
+          console.log('passei no primeiro if');
+        });
+    }
     if (FiltroSpecies !== '' && FiltroStatus === '' && Nome === '') {
       http.get<IFiltro>(`character/?species=${FiltroSpecies}&page=${Page}`)
         .then(resp => {
           setObj(resp.data.results);
           setContador(resp.data.info.pages);
           setContadorObjetos(resp.data.info.count);
-          console.log(contadorObjetos);
-          console.log(contador);
+          console.log('passei no if de somente specie modificada');
+
         }
         );
     }
-    if (FiltroStatus !== '') {
+    if (FiltroStatus !== '' && FiltroSpecies === '' && Nome === '') {
       http.get<IFiltro>(`character/?status=${FiltroStatus}&page=${Page}`)
         .then(resp => {
           setObj(resp.data.results);
           setContador(resp.data.info.pages);
           setContadorObjetos(resp.data.info.count);
+          console.log('passei no if de somente specie modificada');
         }
         );
     }
-    if (FiltroSpecies !== 'all' && FiltroStatus === 'all') {
+
+    if (FiltroSpecies !== '' && FiltroStatus !== '' && Nome === '') {
       http.get<IFiltro>(`character/?status=${FiltroStatus}&species=${FiltroSpecies}&page=${Page}`)
         .then(resp => {
           setObj(resp.data.results);
           setContador(resp.data.info.pages);
           setContadorObjetos(resp.data.info.count);
-        }
-        );
-    }
-    if (FiltroSpecies !== 'all' && FiltroStatus !== 'all' && Nome === '') {
-      http.get<IFiltro>(`character/?status=${FiltroStatus}&species=${FiltroSpecies}&page=${Page}`)
-        .then(resp => {
-          setObj(resp.data.results);
-          setContador(resp.data.info.pages);
-          setContadorObjetos(resp.data.info.count);
+          console.log('passei no if de specie e status modificada');
         }
         );
     }
@@ -72,6 +72,7 @@ export const Home = () => {
           setObj(resp.data.results);
           setContador(resp.data.info.pages);
           setContadorObjetos(resp.data.info.count);
+          console.log('passei no if de todos modificados');
         }
         );
     }
@@ -81,6 +82,28 @@ export const Home = () => {
           setObj(resp.data.results);
           setContador(resp.data.info.pages);
           setContadorObjetos(resp.data.info.count);
+          console.log('passei no if de nome modificado');
+        }
+        );
+    }
+    if (Nome !== '' && FiltroSpecies !== 'all' && FiltroStatus === 'all') {
+      http.get<IFiltro>(`character/?name=${Nome}&page=${Page}`)
+        .then(resp => {
+          setObj(resp.data.results);
+          setContador(resp.data.info.pages);
+          setContadorObjetos(resp.data.info.count);
+          console.log('passei no if de nome e especie modificado');
+        }
+        );
+    }
+    if (Nome !== '' && FiltroSpecies === 'all' && FiltroStatus !== 'all') {
+      http.get<IFiltro>(`character/?name=${Nome}&page=${Page}`)
+        .then(resp => {
+          setObj(resp.data.results);
+          setContador(resp.data.info.pages);
+          setContadorObjetos(resp.data.info.count);
+          console.log('passei no if de nome e status modificado');
+
         }
         );
     }
@@ -95,13 +118,13 @@ export const Home = () => {
       setPage(contador);
     }
 
-    // Logica dos botões com personagens
+    // Logica dos botões para verificar se há personagens necessarios para ir para proxima página
 
-    // contadorObjetos < 20 ? setMaisPersonagens(false) : setMaisPersonagens(true);
-
-    // contadorObjetos === 0 ? setSemPersonagens(true) : setSemPersonagens(false);
+    contadorObjetos < 20 ? setMaisPersonagens(false) : setMaisPersonagens(true);
 
   }, [Page, FiltroSpecies, FiltroStatus, Nome],);
+
+  console.log(contador);
 
   const handleSelectStatus = (e: any) => {
     setPage(1);
@@ -137,7 +160,7 @@ export const Home = () => {
             <Option value='alive' >Alive</Option>
             <Option value='dead' >Dead</Option>
             <Option value='unknown'>Unknown</Option>
-            <Option value='all'>All</Option>
+            <Option value=''>All</Option>
           </Select>
           <Select
             variant="outlined"
@@ -155,45 +178,41 @@ export const Home = () => {
             <Option value='robot'>Robot</Option>
             <Option value='unknown'>Unknown</Option>
             <Option value='animal'>Animal</Option>
-            <Option value='all'>All</Option>
+            <Option value=''>All</Option>
           </Select>
         </div>
       </div>
-      {SemPersonagens ? <section>
-        <p>There is no such Characters, You are a crazy person</p>
-      </section> :
-        <section className='w-screen bg-gray-200 dark:bg-gray-900'>
-          <div className='py-10 px-10 grid gap-3 grid-cols-4  justify-items-center'>
-            {Obj.map(item =>
-              <div key={item.id}
-                className='w-96 bg-teal-600 dark:bg-green-800 flex flex-col items-center rounded-2xl px-2 py-2 flex-wrap'>
-                <div>
-                  <img src={item.image} className='pt-3 rounded-2xl w-80' />
+      <section className='w-screen bg-gray-200 dark:bg-gray-900'>
+        <div className='py-10 px-10 grid gap-3 grid-cols-4  justify-items-center'>
+          {Obj.map(item =>
+            <div key={item.id}
+              className='w-96 bg-teal-600 dark:bg-green-800 flex flex-col items-center rounded-2xl px-2 py-2 flex-wrap'>
+              <div>
+                <img src={item.image} className='pt-3 rounded-2xl w-80' />
+              </div>
+              <div className='pt-3'>
+                <p className='text-2xl font-MontSerrat text-center dark:text-gray-200'>{item.name}</p>
+              </div>
+              <div className='flex py-2'>
+                <div className='flex flex-col w-40 items-center'>
+                  <p className='dark:text-gray-200'>Status:</p>
+                  <p className='text-xl font-medium font-MontSerrat dark:text-gray-200'>{item.status}</p>
                 </div>
-                <div className='pt-3'>
-                  <p className='text-2xl font-MontSerrat text-center dark:text-gray-200'>{item.name}</p>
-                </div>
-                <div className='flex py-2'>
-                  <div className='flex flex-col w-40 items-center'>
-                    <p className='dark:text-gray-200'>Status:</p>
-                    <p className='text-xl font-medium font-MontSerrat dark:text-gray-200'>{item.status}</p>
-                  </div>
-                  <div className='flex flex-col w-40 items-center'>
-                    <p className='dark:text-gray-200'>Specie:</p>
-                    <p className='text-xl font-medium font-MontSerrat text-center dark:text-gray-200'>{item.species}</p>
-                  </div>
-                </div>
-                <div className='flex pb-2 flex-col'>
-                  <div className='flex flex-col items-center py-1'>
-                    <p className='dark:text-gray-200'>Origin:</p>
-                    <p className='text-xl font-medium font-MontSerrat dark:text-gray-200'>{item.origin.name}</p>
-                  </div>
+                <div className='flex flex-col w-40 items-center'>
+                  <p className='dark:text-gray-200'>Specie:</p>
+                  <p className='text-xl font-medium font-MontSerrat text-center dark:text-gray-200'>{item.species}</p>
                 </div>
               </div>
-            )}
-          </div>
-        </section>
-      }
+              <div className='flex pb-2 flex-col'>
+                <div className='flex flex-col items-center py-1'>
+                  <p className='dark:text-gray-200'>Origin:</p>
+                  <p className='text-xl font-medium font-MontSerrat dark:text-gray-200'>{item.origin.name}</p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
       {MaisPersonagens &&
         <div className='flex justify-center pb-5 gap-16 bg-gray-200 dark:bg-gray-900 items-center'>
           <Button
